4ae19e5e37c4f56aa688332b9d90991a137d5c2e
homer <homer@labs2160>
2018-08-22 11:09:57 +0900

    Updating core code line

---
+++ module-core/src/main/java/io/labs2160/vertx/core/RoutingContextExtensions.kt
@@ -0,0 +1,24 @@
+package io.labs2160.vertx.core
+
+import io.netty.handler.codec.http.HttpResponseStatus
+import io.vertx.core.json.Json
+import io.vertx.ext.web.api.RequestParameters
+import io.vertx.ext.web.RoutingContext
+
+fun RoutingContext.params(): RequestParameters = this.get<Any>("parsedParameters") as RequestParameters
+
+fun RoutingContext.respondPretty(status: HttpResponseStatus, body: Any) {
+    this.respond(status, body, true)
+}
+
+fun RoutingContext.respond(status: HttpResponseStatus, body: Any? = null, pretty: Boolean = false) {
+    val response = this.response()
+
+    response.statusCode = status.code()
+
+    if(body == null) {
+        response.end()
+    } else {
+        response.end(if(pretty) Json.encodePrettily(body) else Json.encode(body))
+    }
+}
